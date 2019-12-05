3b70e8f09fe8ecdecb7479761c0eb8e45caee8ce
yuebanbazi <359454330@qq.com>
2019-12-05 10:55:28 +0800

    第一次提交

---
+++ springboot03/src/test/java/com/ybbz/springboot03/Springboot03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybbz.springboot03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
